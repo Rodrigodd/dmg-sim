893fbb5fc85cede70f65784fe97ceb7bdb1ee123
Rodrigo Batista de Moraes <rodrigobatsmoraes@hotmail.com>
2025-01-04 19:27:34 -0300

    fixup: trireg

--- dmg_cpu_b/cells/trireg_m.sv
+++ dmg_cpu_b/cells/trireg_m.sv
@@ -4,7 +4,7 @@
 // SystemVerilog would be a better representation for it, but that is not
 // supported by Icarus Verilog.
 module trireg_m (inout tri logic a);
-	logic a_cap;
+	logic a_cap = /*random*/0;
 	always @(a) a_cap = a;
 	assign (weak1, weak0) a = a_cap;
 endmodule // BusKeeper
